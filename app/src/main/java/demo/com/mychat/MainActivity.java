49a1b1cce4b0bfc211cad015e2621ddd144f730d
7R <4a2k0055@stust.edu.tw>
2017-11-27 16:33:37 +0800

    Initial commit

---
+++ app/src/main/java/demo/com/mychat/MainActivity.java
@@ -0,0 +1,29 @@
+package demo.com.mychat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.editText);
+        listView = (ListView)findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, R.layout.simple_list_item_1,R.id.text1);
+        listView.setAdapter(adapter);
+    }
+
+    public void onClick(View v) {
+        adapter.add(editText.getText().toString());
+    }
+}
